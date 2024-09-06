24b367d4f17a1be9ed5021b7fd080e67e24e64d8
X <gregor.pracek@gatehub.net>
2024-09-06 22:16:18 +0200

    Update node to v0.6.6 Updated go versions using v1.22 Updated with new repo

--- songbird.sh
+++ songbird.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export FBA_VALs=/flare/scripts/configs/songbird/validators.json
-/flare/flare --network-id=songbird --http-host=0.0.0.0 \
+/flare/avalanchego \
+  --network-id=songbird \
+  --http-host=0.0.0.0 \
+  --data-dir=/flare/.flare \
   --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.ip")" \
   --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.nodeID")"
